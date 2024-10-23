8588aa1cff6f1b05b8161ab257e55ce59cc4e325
Jordan <jordanbenbelaid@hotmail.com>
2024-10-23 20:37:52 +0100

    garage task solutions

---
+++ src/com/qa/runner/Runner.java
@@ -0,0 +1,33 @@
+package com.qa.runner;
+
+import com.qa.garage.Garage;
+import com.qa.vehicles.Car;
+import com.qa.vehicles.Motorbike;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		Car carOne = new Car(1, 4, 5, "Blue", "BMW", 2, true);
+		Car carTwo = new Car(2, 4, 3, "Red", "Audi", 4, false);
+		
+		Motorbike bikeOne = new Motorbike(3, 2, 0, "Black", "BMW", 200);
+		Motorbike bikeTwo = new Motorbike(4, 2, 0, "White", "Harley Davidson", 130);
+		
+		Garage garage = new Garage();
+		
+		garage.addVehicle(bikeTwo);
+		garage.addVehicle(bikeOne);
+		garage.addVehicle(carOne);
+		garage.addVehicle(carTwo);
+		
+		//garage.calculateBillById(2);
+		//garage.calculateTotalBill();
+		//garage.removeVehicleByType("Audi");
+		garage.vehicleTypeBill(carTwo);
+		
+		
+		garage.printAll();
+		
+	}
+
+}
